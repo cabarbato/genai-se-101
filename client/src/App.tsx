c2d86cded484b80570e36e905975044aabfabea5
Colleen Barbato <cawalizer@gmail.com>
2024-04-15 22:01:40 -0400

    add pure css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,14 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type { Component } from "solid-js";
+import "purecss"
+import styles from "./App.module.css";
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <header class={styles.header}>Demo Chatbot</header>
+      <main>
+        <form class="pure-form"><input placeholder="Type your question here"></input></form>
+      </main>
     </div>
   );
 };
